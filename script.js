1462731c5cb7dde45105e162e1b68f6992712727
Fabian Bösiger <boefabia@student.ethz.ch>
2023-11-11 12:40:57 +0100

    changes

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+function onScroll() {
+    document.querySelectorAll("#timeline .event").forEach(function(elem) {
+        let elemRect = elem.getBoundingClientRect();
 
-    function onScroll() {
-        document.querySelectorAll("#timeline .event").forEach(function(elem) {
-            let elemRect = elem.getBoundingClientRect();
-    
-            let scrollPos = window.scrollY / (document.body.getBoundingClientRect().height - window.innerHeight);
-            scrollPos = Math.min(Math.max(scrollPos, 0), 1);
-            let focusHeight = window.innerHeight * scrollPos;
-    
-            if (focusHeight >= Math.floor(elemRect.top) && focusHeight <= Math.ceil(elemRect.bottom)) {
-                elem.classList.add("focused");
-            } else {
-                elem.classList.remove("focused");
-            }
-        });
-    }
-    
+        let scrollPos = window.scrollY / (document.body.getBoundingClientRect().height - window.innerHeight);
+        scrollPos = Math.min(Math.max(scrollPos, 0), 1);
+        let focusHeight = window.innerHeight * scrollPos;
+
+        if (focusHeight >= Math.floor(elemRect.top) && focusHeight <= Math.ceil(elemRect.bottom)) {
+            elem.classList.add("focused");
+        } else {
+            elem.classList.remove("focused");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
     onScroll();
-    document.addEventListener("scroll", onScroll);
-    
+    document.addEventListener("scroll", onScroll);  
 })
