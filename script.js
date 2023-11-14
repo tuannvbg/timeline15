b4bebb9b6d589c1c229c62aaac7d970bb10ac097
Fabian Bösiger <boefabia@student.ethz.ch>
2023-11-14 10:55:21 +0100

    change animation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function onScroll() {
     let scrollPos = window.scrollY / (document.body.getBoundingClientRect().height - window.innerHeight);
     scrollPos = Math.min(Math.max(scrollPos, 0), 1);
@@ -7,6 +8,23 @@ function onScroll() {
         let elemRect = elem.getBoundingClientRect();
 
         if (focusHeight >= Math.floor(elemRect.top) && focusHeight <= Math.ceil(elemRect.bottom)) {
+            elem.classList.add("focused");
+        } else {
+            elem.classList.remove("focused");
+        }
+    });
+}
+*/
+
+function onScroll() {
+    let upperFocusHeight = window.innerHeight / 3;
+    let lowerFocusHeight = window.innerHeight / 3 * 2;
+
+
+    document.querySelectorAll("#timeline .event").forEach(function(elem) {
+        let elemRect = elem.getBoundingClientRect();
+
+        if (lowerFocusHeight >= Math.floor(elemRect.top) && upperFocusHeight <= Math.ceil(elemRect.bottom)) {
             elem.classList.add("focused");
         } else {
             elem.classList.remove("focused");
